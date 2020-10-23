f112e8c3530a82e4e735f6bea0af438ca5caa16d
Leire Roa <lroa@plaiaundi.com>
2020-10-23 12:54:01 +0200

    Edicion sin verificar de dat.dart

--- lib/providers/data.dart
+++ lib/providers/data.dart
@@ -5,18 +5,14 @@ import 'dart:io';
 // Cuadro a dibujar
 class Cuadro {
   int nhuecos; // numero de huecos que ocupa
-  int clase; // -1 no asigando
+  int clase;   // -1 no asigando
   int minutos; // numero de minutos asignados
   Cuadro(this.nhuecos, [this.minutos = 0, this.clase = 0]);
 
   @override
-  String toString() {
-    //return '$size; $minutes; $type; $color';
-    return '$nhuecos/$minutos';
-  }
-
+  String toString() => '$nhuecos/$minutos';
+  
   Cuadro.fromString(String s) {
-    //Bloc(0);
     var member = s.split('/');
     this.nhuecos = int.parse(member[0]);
     this.minutos = int.parse(member[1]);
@@ -24,106 +20,117 @@ class Cuadro {
 }
 
 class HorarioData extends ChangeNotifier {
-  // hora inicial
-  final h0 = Duration(hours: 8, minutes: 0);
-
-  // tamaño de 'huecos' en minutos
-  var huecos = [15, 40, 55, 25, 30, 55, 25, 55, 55, 55, 35]; //11
-
-  // almacena los cuadros asignados en el horario de Lunes a Viernes
-  var horario = List<List<Cuadro>>(5);
-
-  // lista de cantidad e huecos por bloque asignado
-  var huecosBloque = [1, 2, 3, 1, 2, 1, 1];
-  //var huecosBloque = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-  // lista de tamaño de los bloques en minutos
-  var minutosBloque = <int>[];
-
+  // ===============  MIEMBROS ==============
   // Repositorio File
   HorarioStorage storage;
 
+  // hora inicial
+  final h0 = Duration(hours: 8, minutes: 0);
+
+  // Tamaño de 'huecos' en minutos
+  var huecos = [15, 40, 55, 25, 30, 55, 25, 55, 55, 55, 35]; //11
+
+  // Almacena los cuadros asignados en el horario de Lunes a Viernes
+  var horario = List<List<Cuadro>>(5);
+
+  // ===============  METODOS ================
+  /// Calcula los minutos de un cuadro
+  /// desde el ihueco y de una longitud nhuecos
+  int mMinutos(ihueco, nhuecos) {
+    var minutos = 0;
+    for (var i = ihueco; i < ihueco + nhuecos; i++) minutos += huecos[i];
+    return minutos;
+  }
+
+  // Pongo en un día tantos cuadros como huecos tenemos = Reset
+  void resetDia(dia) {
+    horario[dia] = [];
+    huecos.forEach((minutos) => horario[dia].add(Cuadro(1, minutos)));
+  }
+
+  // Me recalcula cuadros en minutos del día d
+  void recalculaMinutosDia(var dia) {
+    var iHueco =0;
+    horario[dia].asMap().forEach((idx, cuadro) {
+      cuadro.minutos = nMinutos(iHueco, cuadro.nhuecos);
+      iHueco+=cuadro.nhuecos;
+    });
+  }
+
+  /// Reasignamos un Cuadro 
+  /// El Cuadro debe estar vacio (nhuecos=1 y clase=-1)
+  void reasignaCuadro(int dia, int iCuadro, int nHuecos) {
+    assert(horario[diad][iCuadro].nhuecos == 1);
+    assert(horario[dia][iCuadro].clase == -1);
+    // la longitud asignado <= total cuadros del día
+    assert((iCuadro + nHuecos) <= horario[diad].length);
+
+    var list = <Cuadro>[];
+    var nuevonhuecos = 0;
+    var nuevominutos = 0;
+    for (var i = ciCuadro; i < iCuadro + nHuecoss; i++) {
+      nuevonhuecos += horario[d][i].nhuecos;
+      nuevominutos += horario[d][i].minutos;
+    }
+    for (var i = 0; i < horario[dia].length; i++) {
+      var cuadro;
+
+      if (i == c) {
+        cuadro = Cuadro(nuevonhuecos, nuevominutos); //, -1, 0);
+        i += s - 1; //salta los cuadros que este agrupa
+      } else
+        cuadro = horario[dia][i];
+
+      list.add(cuadro);
+    }
+    horario[dia] = list;
+
+    notifyListeners();
+    storage.writeHorario(minutosBloque);
+  }
+
+  /// Eliminamos un Cuadro de un día
+  /// El Bloque no está vacio ( type<>-1)
+  /// Se crean tantos nuevos bloques como su size
+  void quitarCuadro(int dia,  int iCuadro) {
+    var nuevaLista = <Cuadro>[];
+    var iHueco = 0;
+
+    for (var i = 0; i < horario[dia].length; i++) {
+      if (i == iCuadro) {
+        for (var k = 0; k < horario[dia][i].nhuecos; k++) {
+          nuevaLista.add(Cuadro(1,  nMinutos(iHueco, 1)));
+          iHueco += 1;
+        }
+      } else {
+        nuevaLista.add(horario[dia][i]);
+        iHueco += horario[dia][i].nhuecos;
+      }
+    }
+    horario[dia] = nuevaLista;
+
+    notifyListeners();
+  }
+
+  // ===================   CONSTRUCTOR =======================
   HorarioData() {
     print('Constructor');
 
     storage = HorarioStorage();
-    storage.readHorario().then((l) {
+    storage.leerHorario().then((h) {
       print('==== from storage =====');
-      print(l);
+      print(h);
     });
 
     // print(huecos);
     // print(huecosBloque);
     this.ABORRARcalcularMinutosBloque();
+    huecosACuadrosDia(1);
+    recalculaMinutosDia(0)
 
     print(minutosBloque);
-    printHoras();
   }
 
-  void printHoras() {
-    var t = h0;
-    for (var i = 0; i < huecos.length; i++) {
-      print(t);
-      t += Duration(minutes: huecos[i]);
-    }
-    print(t);
-  }
-
-  void ABORRARcalcularMinutosBloque() {
-    assert(huecos.length == huecosBloque.reduce((a, b) => a + b));
-    num i = 0; // indice
-    num s = 0; // size
-    minutosBloque.clear();
-    huecosBloque.forEach((num o) {
-      for (var j = i; j < i + o; j++) s += huecos[j];
-      i += o;
-
-      minutosBloque.add(s);
-
-      s = 0;
-    });
-    // print('====');
-    // print(huecos);
-    // print(huecosBloque);
-    // print(minutosBloque);
-    // print(minutosBloque.reduce((a, b) => a + b));
-    // print(huecos.reduce((a, b) => a + b));
-
-    assert(minutosBloque.reduce((a, b) => a + b) ==
-        huecos.reduce((a, b) => a + b));
-    notifyListeners();
-    storage.writeHorario(minutosBloque);
-  }
-
-  void quitarBloque(int i) {
-    var nuevosHuecosBloque = <int>[];
-    for (var j = 0; j < huecosBloque.length; j++) {
-      if (j == i)
-        nuevosHuecosBloque
-            .addAll(List<int>.generate(huecosBloque[i], (i) => 1));
-      else
-        nuevosHuecosBloque.add(huecosBloque[j]);
-    }
-    huecosBloque = nuevosHuecosBloque;
-    calcularMinutosBloque();
-
-    notifyListeners();
-  }
-
-  void asignarBloque(int i, int s) {
-    var nuevosHuecosBloque = <int>[];
-
-    for (var j = 0; j < huecosBloque.length; j++) {
-      if (j == i) {
-        nuevosHuecosBloque.add(s);
-        j += s - 1;
-      } else
-        nuevosHuecosBloque.add(huecosBloque[j]);
-    }
-    huecosBloque = nuevosHuecosBloque;
-    calcularMinutosBloque();
-
-    notifyListeners();
-  }
 }
 
 // ========================================
@@ -141,7 +148,7 @@ class HorarioStorage {
     return File('$path/Horario.txt');
   }
 
-  Future<List<int>> readHorario() async {
+  Future<List<int>> leerHorario() async {
     try {
       final file = await _localFile;
 
@@ -154,7 +161,7 @@ class HorarioStorage {
     }
   }
 
-  Future<File> writeHorario(List<int> l) async {
+  Future<File> escribirHorario(List<int> l) async {
     final file = await _localFile;
 
     // Write the file
